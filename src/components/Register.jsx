78ee250cde1684367b4689af4e7176381aff3567
TearsOprah <qmeshokp@mail.ru>
2023-03-29 18:07:05 +0300

    feat: верстка Register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,16 @@
+import {Link} from "react-router-dom";
 
 
 export default function Register() {
   return (
-    <h2>Регистрация</h2>
+    <div className={'login'}>
+      <h2 className={'login__title'}>Регистрация</h2>
+      <form className={'login__form'}>
+        <input className={'login__input'} type={'email'} placeholder={'Email'}/>
+        <input className={'login__input'} type={'password'} placeholder={'Пароль'}/>
+        <button className={'login__submit-button'} type={"submit"}>Зарегистрироваться</button>
+      </form>
+      <p className={'login__bottom-row'}>Уже зарегистрированы? <Link className={'login__link'} to={'/sign-in'}>Войти</Link></p>
+    </div>
   )
 }
