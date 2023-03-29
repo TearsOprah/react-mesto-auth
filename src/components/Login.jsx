d5be1b7813b8764821271b481bb35405974a5d70
TearsOprah <qmeshokp@mail.ru>
2023-03-29 18:08:58 +0300

    feat: верстка Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,13 @@
 
 export default function Login() {
   return (
-    <h2>Логин</h2>
+    <div className={'login'}>
+      <h2 className={'login__title'}>Вход</h2>
+      <form className={'login__form'}>
+        <input className={'login__input'} type={'email'} placeholder={'Email'}/>
+        <input className={'login__input'} type={'password'} placeholder={'Пароль'}/>
+        <button className={'login__submit-button'} type={"submit"}>Войти</button>
+      </form>
+    </div>
   )
 }
